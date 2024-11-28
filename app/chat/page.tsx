54ea6317a5ab33c798333afc804ff215923d6303
Sergio-Cerrillo <sergiocerrilloruiz@gmail.com>
2024-11-28 12:26:33 +0100

    Extract generic Button component

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,36 +1,35 @@
 "use client"
 import React from 'react';
-import InputField from '../ui/Input'; 
-import SendButton from '../ui/SendButton';   
-import DeleteButton from '../ui/DeleteButton';  
-import useChat from '../hooks/useChat';  
+import InputField from '../ui/Input';
+import useChat from '../hooks/useChat';
 import Footer from '../ui/Footer';
+import Button from '../ui/Button';
 
 const Chat: React.FC = () => {
-  const { messages,message, setMessage, sendMessage, clearChat } = useChat(); 
+  const { messages, message, setMessage, sendMessage, clearChat } = useChat();
 
   return (
     <>
       <section className="chat">
-        <DeleteButton onClick={clearChat} />  
-       
+        <Button className="delete-button" onClick={clearChat}><span>Borrar Chat</span></Button>
+
         <ul className="messages">
-              {messages.map((msg, index) => (
-              <li key={index}>{msg}</li>
+          {messages.map((msg, index) => (
+            <li key={index}>{msg}</li>
           ))}
         </ul>
 
         <form className="form" onSubmit={(e) => e.preventDefault()}>
-          <InputField 
+          <InputField
             placeholder="Escribe tu m£ensaje"
             value={message}
-            onChange={(e) => setMessage(e.target.value)} 
+            onChange={(e) => setMessage(e.target.value)}
           />
-          <SendButton onClick={sendMessage} />  
+          <Button className="button" onClick={sendMessage}><span>Enviar</span></Button>
         </form>
       </section>
 
-        <Footer/>
+      <Footer />
     </>
   );
 };
